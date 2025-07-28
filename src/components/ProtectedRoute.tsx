3a76b7c765ba6096258b128fcbada2e4b2378bd9
Arefe-Ehyaee <arefe.ehyaee@gmail.com>
2025-07-28 08:51:26 +0330

    protected routes toast fixed

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,13 +1,25 @@
-// components/ProtectedRoute.tsx
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import useUserStore from '../store/UserStore';
+import { toast } from 'react-toastify';
+import { useEffect, useRef } from 'react';
 
 const ProtectedRoute = () => {
   const user = useUserStore((state) => state.user);
+  const location = useLocation();
+  const toastShown = useRef(false); // Prevent multiple toasts
 
-  // If user is not logged in, redirect to login
+  useEffect(() => {
+    if (!user && !toastShown.current) {
+      toast.warn("برای دسترسی به این بخش، ابتدا وارد حساب کاربری شوید.", {
+          className: 'toast',
+          progressClassName: 'fancy-progress-bar',
+        });
+      toastShown.current = true;
+    }
+  }, [user]);
+
   if (!user) {
-    return <Navigate to="/login" replace />;
+    return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
   return <Outlet />;
